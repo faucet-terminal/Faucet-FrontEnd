381bca4e2700614584d40cfc6cbd17142777d75a
finch ren <344919881@qq.com>
2024-05-29 00:58:35 +0800

    feat: credential

--- actions/register.ts
+++ actions/register.ts
@@ -1,13 +1,13 @@
 "use server";
 
 import * as z from "zod";
-// import bcrypt from "bcryptjs";
+import bcrypt from "bcryptjs";
 
 import { db } from "@/lib/db";
 import { RegisterSchema } from "@/schemas";
 import { getUserByEmail } from "@/data/user";
 // import { sendVerificationEmail } from "@/lib/mail";
-import { generateVerificationToken } from "@/lib/tokens";
+// import { generateVerificationToken } from "@/lib/tokens";
 
 export const register = async (values: z.infer<typeof RegisterSchema>) => {
   const validatedFields = RegisterSchema.safeParse(values);
@@ -15,31 +15,34 @@ export const register = async (values: z.infer<typeof RegisterSchema>) => {
   if (!validatedFields.success) {
     return { error: "Invalid fields!" };
   }
-  // todo 等邮箱服务
-  // const { email, password } = validatedFields.data;
-  // const hashedPassword = await bcrypt.hash(password, 10);
 
-  // const existingUser = await getUserByEmail(email);
+  const { email, password } = validatedFields.data;
+  const hashedPassword = await bcrypt.hash(password, 10);
 
-  // if (existingUser) {
-  //   return { error: "Email already in use!" };
-  // }
+  const existingUser = await getUserByEmail(email);
 
-  // await db.user.create({
-  //   data: {
-  //     email,
-  //     password: hashedPassword,
-  //   },
-  // });
+  if (existingUser) {
+    return { error: "Email already in use!" };
+  }
+
+  await db.user.create({
+    data: {
+      email,
+      password: hashedPassword,
+    },
+  });
 
   // const verificationToken = await generateVerificationToken(email);
   // const sendRes = await sendVerificationEmail(
   //   verificationToken.email,
   //   verificationToken.token,
   // );
-
-  // if (sendRes?.error) {
-  //   return { error: sendRes.error }
-  // }
+  // todo 等邮箱服务
+  const sendRes = {
+    error: 'todo email service'
+  }
+  if (sendRes?.error) {
+    return { error: sendRes.error }
+  }
   return { success: "Confirmation email sent!" };
 };
--- auth.ts
+++ auth.ts
@@ -2,11 +2,14 @@ import NextAuth from "next-auth"
 import { PrismaAdapter } from "@auth/prisma-adapter";
 import Github from "next-auth/providers/github";
 import Google from "next-auth/providers/google";
+import Credentials from "next-auth/providers/credentials";
 import { UserRole } from "@prisma/client";
+import bcrypt from "bcryptjs";
 
 import { db } from "@/lib/db";
-import { getUserById } from "./data/user";
+import { getUserByEmail, getUserById } from "@/data/user";
 import { getAccountByUserId } from "./data/account";
+import { LoginSchema } from "./schemas";
 
 export const {
   handlers: { GET, POST },
@@ -28,7 +31,27 @@ export const {
       clientId: process.env.GITHUB_CLIENT_ID,
       clientSecret: process.env.GITHUB_CLIENT_SECRET,
     }),
-    // todo Credentials
+    Credentials({
+      async authorize(credentials) {
+        const validatedFields = LoginSchema.safeParse(credentials);
+
+        if (validatedFields.success) {
+          const { email, password } = validatedFields.data;
+          
+          const user = await getUserByEmail(email);
+          if (!user || !user.password) return null;
+
+          const passwordsMatch = await bcrypt.compare(
+            password,
+            user.password,
+          );
+
+          if (passwordsMatch) return user;
+        }
+
+        return null;
+      }
+    })
   ],
   events: {
     async linkAccount({ user }) {
