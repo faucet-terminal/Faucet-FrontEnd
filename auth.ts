cd69cab7683f4b9d3725074155d41a2c0c8e654a
finch ren <344919881@qq.com>
2024-05-31 22:39:09 +0800

    fix: github login=> allowDangerousEmailAccountLinking

--- auth.ts
+++ auth.ts
@@ -30,6 +30,7 @@ export const {
     Github({
       clientId: process.env.GITHUB_CLIENT_ID,
       clientSecret: process.env.GITHUB_CLIENT_SECRET,
+      allowDangerousEmailAccountLinking:true,
     }),
     Credentials({
       async authorize(credentials) {
