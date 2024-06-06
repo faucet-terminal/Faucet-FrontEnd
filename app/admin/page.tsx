8425495ab61af238ac60a93cee0b4208da823799
finch ren <344919881@qq.com>
2024-06-07 01:46:21 +0800

    fix: admin auth

--- app/admin/page.tsx
+++ app/admin/page.tsx
@@ -1,6 +1,18 @@
+import { auth } from "@/auth";
 import AdminTable from "@/components/admin/crypto-currency-table";
+import { Button } from "@nextui-org/button";
+import { UserRole } from "@prisma/client";
+import Link from "next/link";
 
-const AdminPage = () => {
+const AdminPage = async () => {
+  const authInfo = await auth()
+  if (authInfo?.user.role !== UserRole.ADMIN) {
+    return <div className="text-center">
+      <h1 className="bold text-xl">Access Denied</h1>
+      <h2>You do not have the necessary permissions to view this page.</h2>
+      <Button as={Link} href="/" className="my-6">Back Home</Button>
+    </div>
+  }
   return <AdminTable />;
 };
 
